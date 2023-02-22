6c74ce92536f9a007bc88b325edffc9f183619ab
Miodrag <mmitik@bitsia.com>
2023-02-22 02:00:19 +0100

    testing commit

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -9,7 +9,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('hello')
-  getData(): Message {
+  getData() : Message {
     return this.appService.getData();
   }
   @Get('welcome')
