38851fb8ec1592aa160b112c19342c09cbe6a1a8
Miki <mmitik@bitsia.com>
2023-02-23 09:32:15 +0100

    forcing vercel redeploy

--- apps/angular-nest/src/app/app.component.ts
+++ apps/angular-nest/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Message } from '@angular-nest/api-interfaces';
 
 @Component({
-  selector: 'angular-nest-root',
+  selector: 'angular-nest-root' ,
   templateUrl: './app.component.html',
   styleUrls:  ['./app.component.scss'],
 })
